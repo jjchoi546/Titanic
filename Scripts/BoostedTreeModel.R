8312487a780862fff178ff0a46025dd8fab140ed
John Choi <jjchoi@Johns-MacBook-Pro.local>
2021-10-01 23:29:40 -0700

    gbm update

--- Scripts/BoostedTreeModel.R
+++ Scripts/BoostedTreeModel.R
@@ -46,7 +46,7 @@ folds <- sample(1:5, nrow(train_data), replace = TRUE)
 set.seed(1)
 
 Boost.model <- gbm(formula = Survived ~ ., 
-                   data = train_data[folds != 1,],
+                   data = train_data,
                    distribution = "bernoulli",
                    n.trees = 10000,
                    interaction.depth = 1,
@@ -57,26 +57,27 @@ Boost.model <- gbm(formula = Survived ~ .,
 print(Boost.model)
 sqrt(min(Boost.model$cv.error))
 gbm.perf(Boost.model, method = 'cv')
+invisible(gc())
 
 set.seed(1)
 Boost2.model <- gbm(formula = Survived ~ ., 
-                   data = train_data[folds != 1,],
+                   data = train_data,
                    distribution = "bernoulli",
-                   n.trees = 5000,
+                   n.trees = 10000,
                    interaction.depth = 3,
-                   shrinkage = 0.1,
+                   shrinkage = 0.001,
                    verbose = FALSE,
                    cv.folds = 5)
 
 print(Boost2.model)
 sqrt(min(Boost2.model$cv.error))
 gbm.perf(Boost2.model, method = 'cv')
+invisible(gc())
 
-
-hyper_grid <- expand.grid(shrinkage = c(.001, .005, .01),
-                         interaction.depth = c(4, 5, 6),
-                         n.minobsinnode = c(5, 7, 10),
-                         bag.fraction = c(.8, .9, 1),
+hyper_grid <- expand.grid(shrinkage = c(.0059, .006, .0061),
+                         interaction.depth = c(3, 4, 5),
+                         n.minobsinnode = c(4, 5, 6),
+                         bag.fraction = 1,
                          optimal_trees = 0,
                          min_RMSE = 0)
 
@@ -85,28 +86,24 @@ for(i in 1:nrow(hyper_grid)) {
   
   Boost.tune <- gbm(formula = Survived~.,
                     distribution = "bernoulli",
-                    data = train_data[folds!=1,],
+                    data = train_data,
                     n.trees = 5000,
                     interaction.depth = hyper_grid$interaction.depth[i],
                     shrinkage = hyper_grid$shrinkage[i],
                     n.minobsinnode = hyper_grid$n.minobsinnode[i],
                     bag.fraction = hyper_grid$bag.fraction[i],
-                    cv.folds = 5,
+                    train.fraction = .8,
+                    n.cores = NULL,
                     verbose = FALSE)
-  hyper_grid$optimal_trees[i] <- which.min(Boost.tune$cv.error)
-  hyper_grid$min_RMSE[i] <- sqrt(min(Boost.tune$cv.error))
+  
+  hyper_grid$optimal_trees[i] <- which.min(Boost.tune$valid.error)
+  hyper_grid$min_RMSE[i] <- sqrt(min(Boost.tune$valid.error))
+  invisible(gc())
 }
 
 hyper_grid %>% dplyr::arrange(min_RMSE) %>% head(10)
-sqrt(min(Boost.tune$cv.error))
-gbm.perf(Boost.tune, method = 'cv')
 
-
-min_MSE <- which.min(Boost.model$cv.error)
-sqrt(Boost.model$cv.error[min_MSE])
-
-Boost.model$cv.error
-
+set.seed(1)
 for (i in 1:k) {
   start_time <- Sys.time()
   
@@ -119,13 +116,14 @@ for (i in 1:k) {
   pred <- predict(Boost.model, 
                   newdata = train_data[folds == i,], 
                   n.trees=5000,
+                  shrinkage = 0.006,
+                  interaction.depth = 3,
+                  n.minobsinnode = 5,
+                  bag.fraction = 1,
                   type = "response")
   
   pred <- ifelse(pred > 0.5, 1, 0)
-  
-  #Where data is NA, rForest cannot predict so just assume survived
-  #pred[is.na(pred)] <- ifelse(train_data$Sex[folds==i][is.na(pred)]=='female', 1, 0)
-  
+
   cv.mse_errors[i] <- mean((as.numeric(as.character(train_data$Survived[folds==i])) - pred)^2)
   cv.sae_errors[i] <- sum(abs(as.numeric(as.character(train_data$Survived[folds==i])) - pred))
   
@@ -137,9 +135,41 @@ for (i in 1:k) {
 cv.Boost_mse <- mean(cv.mse_errors, na.rm = TRUE)
 cv.Boost_sae <- mean(cv.sae_errors, na.rm = TRUE)
 
-Boost.model <- gbm(formula = Survived ~ ., data = train_data, 
-                   distribution = "bernoulli",
-                   n.trees = 5000,
-                   interaction.depth = 4)
 
-summary(Boost.model)
+if (printSubmission) {
+  test_data <- read.csv(paste0("data/test.csv"), header = T, sep = ",")
+  
+  #Convert character features to factor
+  test_data$Sex <- as.factor(test_data$Sex)
+  test_data$Embarked <- as.factor(test_data$Embarked)
+  test_data$Cabin <- substr(test_data$Cabin, 0, 1)
+  test_data$Cabin <- as.factor(test_data$Cabin)
+  
+  Boost.model <- gbm(formula = Survived ~ ., data = train_data, 
+                     distribution = "bernoulli",
+                     n.trees = 5000,
+                     shrinkage = 0.006,
+                     n.minobsinnode = 5,
+                     interaction.depth = 3,
+                     bag.fraction = 1,
+                     type = "response")
+  
+  pred <- predict(Boost.model, 
+                  newdata = test_data, 
+                  n.trees=5000,
+                  shrinkage = 0.006,
+                  interaction.depth = 3,
+                  n.minobsinnode = 5,
+                  bag.fraction = 1,
+                  type = "response")
+  
+  pred <- ifelse(pred > 0.5, 1, 0)
+  
+  test_data$Survived <- pred
+  
+  #Features to print in submission file
+  submission_columns <- c('PassengerId', 'Survived')
+  submission_data <- test_data[submission_columns]
+  write.csv(submission_data, file = "output/Predictions/BoostModel.csv", row.names = FALSE)
+}
+
